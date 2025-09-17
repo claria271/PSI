3aeb29db5d2957745e3eddb65ca4b0d566b26187
Fairuz109 <fairuz.23109@mhs.unesa.ac.id>
2025-09-17 11:06:27 +0700

    update index

--- index.php
+++ index.php
@@ -33,20 +33,21 @@
 </div>
 
       <!-- Bagian teks -->
-      <div class="col-lg-6">
-        <h1 class="display-4 fw-bold lh-1 mb-4">Selamat Datang di Website PSI Kota Surabaya</h1>
-        <p class="lead mb-4">
-          Kita percaya, solidaritas bukan sekadar kata. Melalui program bantuan dana sosial, 
-          PSI berupaya meringankan beban masyarakat yang membutuhkan, dengan proses yang terbuka, cepat, 
-          dan tanpa diskriminasi.
-        </p>
-        <div class="d-grid gap-3 d-md-flex justify-content-md-start">
-          <a href="login.php" class="btn btn-secondary btn-lg px-4">Login</a>
-          <a href="register.php" class="btn btn-outline-secondary btn-lg px-4">Register</a>
-        </div>
-      </div>
-    </div>
+<div class="col-lg-6">
+  <h1 class="display-4 fw-bold lh-1 mb-4">
+    Selamat Datang di Website PSI Kota Surabaya
+  </h1>
+  <p class="lead mb-4 text-justify" style="text-align: justify;">
+    Kita percaya, solidaritas bukan sekadar kata. Melalui program bantuan dana sosial, 
+    PSI berupaya meringankan beban masyarakat yang membutuhkan dukungan, 
+    dengan proses yang terbuka, cepat, dan tanpa diskriminasi.
+  </p>
+  <div class="d-grid gap-3 d-md-flex justify-content-md-start">
+    <a href="login.php" class="btn btn-secondary btn-lg px-4">Login</a>
+    <a href="register.php" class="btn btn-outline-secondary btn-lg px-4">Register</a>
   </div>
+</div>
+
 
   <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
