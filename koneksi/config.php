94bd05f4aee4634bc4c3ce788af8cf25614e3913
ardya6 <rani13resita@gmail.com>
2025-12-28 10:50:06 +0700

    config

--- koneksi/config.php
+++ koneksi/config.php
@@ -1,10 +1,11 @@
-<?php
-$host = "localhost";
-$user = "root"; // default XAMPP
-$pass = "";     // default kosong
-$db   = "web_psi";
 
-$conn = new mysqli($host, $user, $pass, $db);
+<?php
+$host = 'localhost'; // atau IP server database
+$username = 'u433620554_web_psi'; // username database
+$password = '1Kgkentang'; // ISI PASSWORD DATABASE DI SINI
+$database = 'u433620554_nama_database'; // nama database
+
+$conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
